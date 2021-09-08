fae0a1c8822455fe3a63e359c504031a915f00d7
NataliVerbenska <nataliia.verbenska@authenticgaming.com>
2021-09-08 10:07:09 +0300

    send test email

---
+++ src/handlers/sendMail.js
@@ -0,0 +1,33 @@
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES( { region: 'eu-west-1' } )
+
+const sendMail = async (event, context) => {
+  const params = {
+    Source: 'nataly.verbenskaya@gmail.com',
+    Destination: {
+      ToAddresses: ['nataly.verbenskaya@gmail.com'],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: 'Hello from auction!'
+        },
+      },
+      Subject: {
+        Data: 'Test AUCTION Mail'
+      }
+    },
+  };
+
+  try {
+    const result = await ses.sendEmail( params ).promise();
+    console.log(result);
+    return result;
+  } catch ( error ){
+    console.error( error );
+  }
+
+};
+
+export const handler = sendMail;
